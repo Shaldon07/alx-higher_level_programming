4e9b09ebc5b0ea1718c2de97dd9af11957e16980
Shaldon Maarman <shaldonm7@gmail.com>
2023-08-30 15:52:53 -0700

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""Defines class Square."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size):
+        """Initializes new Square.
+        Args:
+            size (int): The size of new square.
+        """
+        self.__size = size
